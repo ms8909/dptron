a25048364957d44caab593b8d2d7c06826b85abd
Your Name <you@example.com>
2019-11-06 23:48:38 +0500

    version 1 added

---
+++ lib/v1/save_file.py
@@ -0,0 +1,10 @@
+# imports
+from lib.logs import logger
+
+class SaveFile(object):
+    def __init__(self):
+        # add variables here
+        pass
+
+    def read(self, address="", local=False, file_format="csv", s3={}):
+        return True
---
+++ lib/v1/save_file_local.py
@@ -0,0 +1,37 @@
+# imports
+from lib.logs import logger
+
+class SaveFileLocal(object):
+    def __init__(self):
+        # add variables here
+        self.temp = ""
+
+    def read(self, address="", file_format="csv"):
+        """
+
+        :param address:
+        :param file_format:
+        :return:
+        """
+        return True
+
+    def save_csv(self):
+        """
+
+        :return:
+        """
+        return True
+
+    def save_excel(self):
+        """
+
+        :return:
+        """
+        return True
+
+    def save_parquet(self):
+        """
+
+        :return:
+        """
+        return True
