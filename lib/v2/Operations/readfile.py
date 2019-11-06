26a13fcc54a3afa04592763835f91d7add33658e
Your Name <you@example.com>
2019-11-06 23:56:42 +0500

    Operations added

---
+++ lib/v2/Operations/readfile.py
@@ -0,0 +1,45 @@
+# imports
+from lib.v2.Operations.read_file_from_local import ReadFileFromLocal
+from lib.v2.Operations.read_file_from_s3 import ReadFileFromS3
+from lib.v2.Logger.logs import logger
+
+
+class ReadFile(object):
+    def __init__(self):
+        # add variables here
+        self.dataframe = ""
+
+    def read(self, address="", local="yes", file_format="csv", s3={}):
+        """
+
+        :param address:
+        :param local:
+        :param file_format:
+        :param s3:
+        :return:
+        """
+        try:
+            if local == "yes":
+                """
+                Time to read the file saved locally
+                """
+                rf = ReadFileFromLocal()
+                self.dataframe= rf.read(address, file_format)
+
+            elif s3 != {}:
+                """
+                Time to read data from s3
+                """
+                self.dataframe = ReadFileFromS3(address, file_format, s3)
+
+            else:
+                """
+                Not sure where the file is saved.
+                """
+                message = "Please make sure you have file saved on either your local system or s3."
+                logger.debug(message)
+                self.dataframe = {"success": False, "message": message}
+
+            return self.dataframe
+        except Exception as e:
+            logger.error(e)
